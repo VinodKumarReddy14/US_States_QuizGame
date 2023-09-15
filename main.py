9aa5bcbfa665e4ee922c0b6e466fdcdff493f0ed
VinodKumarReddy14 <imvinodreddy14@gmail.com>
2023-09-15 12:50:14 +0530

    modification

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ while len(guessed_states) < 50:
     ans = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                            prompt="Enter the State Name ").title()
     if ans == 'Exit':
-        missing_states = []
-        for state in state_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in state_list if state not in guessed_states]
         not_rem_data = p.DataFrame(missing_states)
         not_rem_data.to_csv("states_to_learn.csv")
         break
@@ -27,6 +24,3 @@ while len(guessed_states) < 50:
         state_data = states[states.state == ans]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(ans)
-
-
-
